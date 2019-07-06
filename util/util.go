cc585f0fe8ba55d44401ee46740b1cf2e7cd7acd
frozen <wancsc@163.com>
2019-07-06 22:15:32 +0800

    fix windowsç系统中è获取当前目录ä½ä地址为二进制文件所在目录ç地址

--- util/util.go
+++ util/util.go
@@ -3,10 +3,17 @@ package util
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 // GetCurrentDirectory returns the current path.
 func GetCurrentDirectory() string {
+	if runtime.GOOS == "windows" {
+		path, err := os.Getwd()
+		if err == nil {
+			return path
+		}
+	}
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		return ""
